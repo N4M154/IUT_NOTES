a8aeac40ba1c5ff983905e1a1af9803018ff4963
N4M154 <namisa.najah.raisa@gmail.com>
2024-06-11 11:10:31 +0600

    -_-

---
+++ 3RD_SEMESTER/OOPII_Lab/99_Bottles_of_Beer/99_bottles_of_beer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _99_bottles_of_beer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bottles = 99;
+            while (bottles > 0)
+            {
+                Console.WriteLine($"{bottles} bottles of beer on the wall, {bottles} bottles of beer.");
+                Console.WriteLine("Take one down and pass it around, " + (--bottles > 0 ? $"{bottles}" : "no more") + " bottle" + (bottles != 1 ? "s" : "") + " of beer on the wall.");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("No more bottles of beer on the wall, no more bottles of beer.");
+            Console.WriteLine("Go to the store and buy some more, 99 bottles of beer on the wall.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3RD_SEMESTER/OOPII_Lab/99_Bottles_of_Beer/Bottle_of_beer_test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Bottle_of_beer_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
