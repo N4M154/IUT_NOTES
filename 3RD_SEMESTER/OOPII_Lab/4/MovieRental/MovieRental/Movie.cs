84e4ecd2c496833afd90460e1b263ccd4262841d
N4M154 <namisa.najah.raisa@gmail.com>
2024-06-11 10:39:49 +0600

    -_-

---
+++ 3RD_SEMESTER/OOPII_Lab/4/MovieRental/MovieRental/Movie.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRental
+{
+    /*genre:horror,romantic,thriller and others
+each movie: title,genre,release date,duration
+horror:20%
+romantic:30%      //discount on the base price
+thriller:15%    
+
+thriller>5 yrs old-->15+10 %
+
+base price is decided by the rental service.*/
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+       
+        public DateTime ReleaseDate { get; set; }
+        public int Duration { get; set; }
+        public double BasePrice { get; set; }
+
+        
+
+       public Movie(string title,string genre,DateTime releasedate,int duration,double baseprice)
+        {
+            this.Title= title;
+            this.Genre = genre;
+            this.ReleaseDate = releasedate;
+            this.Duration = duration;
+            this.BasePrice = baseprice;
+        }
+
+        public double RentalPrice()
+        {
+            double discountedPrice = BasePrice;
+            if(Genre=="Horror")
+            {
+                discountedPrice = BasePrice * 0.8; 
+            }
+            else if(Genre=="Romantic")
+            {
+                discountedPrice = BasePrice * 0.7;
+            }
+            else if(Genre=="Thriller")
+            {
+                discountedPrice = BasePrice * 0.85;
+                if((DateTime.Now.Year - ReleaseDate.Year) > 5)
+                {
+                    discountedPrice *= 0.9;
+                }
+                
+            }
+            return discountedPrice;
+
+        }
+        
+
+       
+    }
+}
---
+++ 3RD_SEMESTER/OOPII_Lab/4/MovieRental/MovieRental/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRental
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("Citizen Kane", "Romantic", new DateTime(1960, 2, 12), 120, 19.99);
+
+
+            Movie movie2 = new Movie("Annabelle", "Horror", new DateTime(2015, 5, 23), 90, 18.75);
+
+
+            Movie movie3 = new Movie("Maze Runner", "Thriller", new DateTime(2013, 8, 30), 115, 20.25);
+            
+
+            Console.WriteLine($"First movie discount :{movie1.RentalPrice()} ");
+            Console.WriteLine($"Second movie discount :{movie2.RentalPrice()} ");
+            Console.WriteLine($"Third movie discount :{movie3.RentalPrice()} ");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
