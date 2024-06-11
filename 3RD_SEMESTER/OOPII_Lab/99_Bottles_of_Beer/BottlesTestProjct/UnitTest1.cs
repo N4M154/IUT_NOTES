96b61d05cf3a9feaebd9ef5c9df2b9542afb7d6e
N4M154 <namisa.najah.raisa@gmail.com>
2024-06-11 11:12:06 +0600

    Add files via upload

---
+++ 3RD_SEMESTER/OOPII_Lab/99_Bottles_of_Beer/BottlesTestProjct/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BottlesTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
