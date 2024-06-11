3a13cdefdac3d285b4357206363cc30ded9a2147
N4M154 <namisa.najah.raisa@gmail.com>
2024-06-11 10:38:05 +0600

    -_-

---
+++ 3RD_SEMESTER/OOPII_Lab/4/MovieRental/RefactoredMovieRental/Movie.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoredMovieRental
+{
+    /*genre:horror,romantic,thriller and others
+each movie: title,genre,release date,duration
+horror:20%
+romantic:30%      //discount on the base price
+thriller:15%    
+
+thriller>5 yrs old-->15+10 %
+
+base price is decided by the rental service.*/
+    public abstract class Movie
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+        public int Duration { get; set; }
+        public double BasePrice { get; set; }
+        public Movie(string title, string genre, DateTime releaseDate, int duration, double basePrice)
+        {
+            Title = title;
+            Genre = genre;
+            ReleaseDate = releaseDate;
+            Duration = duration;
+            BasePrice = basePrice;
+        }
+
+        public abstract double RentalPrice();
+    }
+
+    public class Horror: Movie
+    {
+        public Horror(string title, string genre, DateTime releaseDate, int duration, double basePrice) 
+            : base(title, genre, releaseDate, duration, basePrice)
+        {
+
+        }
+        public override double RentalPrice()
+        {
+            return BasePrice * 0.8;
+        }
+    }
+    public class Romantic : Movie
+    {
+        public Romantic(string title, string genre, DateTime releaseDate, int duration, double basePrice) 
+            : base(title, genre, releaseDate, duration, basePrice)
+        {
+
+        }
+        public override double RentalPrice()
+        {
+            return BasePrice * 0.7;
+        }
+    }
+    public class Thriller : Movie
+    {
+        public Thriller(string title, string genre, DateTime releaseDate, int duration, double basePrice) 
+            : base(title, genre, releaseDate, duration, basePrice)
+        {
+
+        }
+        public override double RentalPrice()
+        {
+            double price = BasePrice * 0.85;
+            if ((DateTime.Now.Year - ReleaseDate.Year) > 5)
+            {
+                price*=0.9;
+            }
+
+            return price;
+
+        }
+    }
+
+
+}
---
+++ 3RD_SEMESTER/OOPII_Lab/4/MovieRental/RefactoredMovieRental/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoredMovieRental
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Romantic("Citizen Kane", "Romantic", new DateTime(1960, 2, 12), 120, 19.99);
+
+
+            Movie movie2 = new Horror("Annabelle", "Horror", new DateTime(2015, 5, 23), 90, 18.75);
+
+
+            Movie movie3 = new Thriller("Maze Runner", "Thriller", new DateTime(2013, 8, 30), 115, 20.25);
+            
+
+            Console.WriteLine($"First movie discount :{movie1.RentalPrice()} ");
+            Console.WriteLine($"Second movie discount :{movie2.RentalPrice()} ");
+            Console.WriteLine($"Third movie discount :{movie3.RentalPrice()} ");
+            Console.ReadKey();
+        }
+    }
+}
