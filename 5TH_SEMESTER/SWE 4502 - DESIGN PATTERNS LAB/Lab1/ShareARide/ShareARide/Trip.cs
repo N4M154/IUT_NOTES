b0670c78d95623313651d18f7f92c68551db09c9
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-09-17 20:05:54 +0600

    -_-

---
+++ 5TH_SEMESTER/SWE 4502 - DESIGN PATTERNS LAB/Lab1/ShareARide/ShareARide/Trip.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareARide
+{
+    public class Trip
+    {
+        public int TripId { get; set; }
+        public string PickupLocation { get; set; }
+        public string DropoffLocation {  get; set; }
+        public string Status { get; set; }
+        public RideType RideType { get; set; }
+        public double Distance { get; set; }
+        public double Fare { get; set; }
+
+        public Driver AssignedDriver { get; set; }
+        public NotificationService notificationService;
+        public PaymentMethod paymentMethod;
+
+
+
+
+        public Trip() { }
+
+        public void completeTrip()
+        {
+            Status = "Trip completed";
+            paymentMethod.ProcessPayment(Fare);
+            notificationService.SendNotification("Trip is complete.");
+        }
+
+        public void assignDriver(Driver driver)
+        {
+            
+
+            AssignedDriver = driver;
+            Status = "Driver assigned";
+            notificationService.SendNotification("You've been assigned a driver");
+
+        }
+
+        public double calculateFare(double Distance)
+        {
+            return Distance * RideType.FareCoefficient();
+        }
+
+
+    }
+    /* -_- N4M154 -_- */
+}
