c7d8487bc91294f1ff7cd188d72cbf1e6e271559
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-02-22 23:01:45 +0600

    -_-

---
+++ 5TH_SEMESTER/SWE 4502 - DESIGN PATTERNS LAB/Lab4/IUTBeans/IUTBeans/Condiment/WhippedCream.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IUTBeans
+{
+    internal class WhippedCream:ICondiment
+    {
+        
+
+        public string Name()
+        {
+            return "Whipped Cream";
+        }
+
+        public double Price()
+        {
+            return 30.00;
+        }
+    }
+}
---
+++ 5TH_SEMESTER/SWE 4502 - DESIGN PATTERNS LAB/Lab4/IUTBeans/IUTBeans/Condiment/WhiteSugar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IUTBeans
+{
+    internal class WhiteSugar:ICondiment
+
+    {
+        
+
+        public string Name()
+        {
+            return "White Sugar";
+        }
+
+        public double Price()
+        {
+            return 15.00;
+        }
+    }
+}
