55931e83aa3baac5b87f149adf78c1bacf264bf4
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-02-22 22:58:15 +0600

    -_-

---
+++ 5TH_SEMESTER/SWE 4502 - DESIGN PATTERNS LAB/Lab2_resumption_of_Lab1/ShareARide/ShareARide/Rating.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareARide
+{
+    public class Rating
+    {
+        public User user {  get; set; }
+        public double Value { get; set; }
+        public string Feedback { get; set; }
+
+        public Rating(User u,double value,string feedback)
+        {
+            this.user = u;
+            this.Value = value;
+            this.Feedback = feedback;
+        }
+
+
+    }
+    /* -_- N4M154 -_- */
+}
